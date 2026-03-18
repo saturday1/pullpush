ab71d595622d4904a6f965f635c834c234ae1ea5
Joakim Borg <joakim.borg.1978@gmail.com>
2026-03-18 09:41:15 +0100

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/workout/',
+  base: '/',
   build: {
-    outDir: '../jborg.se/workout',
+    outDir: './dist',
     emptyOutDir: true,
   },
 })
